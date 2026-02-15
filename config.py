4749f62b4736e4ede2786ebdfb0d30376c4367c9
iammohit-kenshin <iamnotmohit.1@gmail.com>
2026-02-15 10:41:06 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.getenv("37407868"))
+API_HASH = os.getenv("d7d3bff9f7cf9f3b111129bdbd13a065")
+BOT_TOKEN = os.getenv("8508688706:AAHHm-0obE0hrRk7hhc2PteptEWmJxYo5ng")
+
+ADMIN_ID = int(os.getenv("6728678197"))
+LOG_GROUP_ID = int(os.getenv("-1003402760539"))  # private storage group id
+
+AUTO_DELETE = int(os.getenv("AUTO_DELETE", "600"))  # seconds
