6d0be1245d28c53ecd8b3f5ca084b2584715900f
iammohit-kenshin <iamnotmohit.1@gmail.com>
2026-02-15 11:43:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("37407868"))
-API_HASH = os.getenv("d7d3bff9f7cf9f3b111129bdbd13a065")
-BOT_TOKEN = os.getenv("8508688706:AAHHm-0obE0hrRk7hhc2PteptEWmJxYo5ng")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMIN_ID = int(os.getenv("6728678197"))
-LOG_GROUP_ID = int(os.getenv("-1003402760539"))  # private storage group id
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))  # private storage group id
 
-AUTO_DELETE = int(os.getenv("AUTO_DELETE", "600"))  # seconds
+AUTO_DELETE = int(os.getenv("AUTO_DELETE", "600"))
