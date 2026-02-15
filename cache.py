0fc21cdd2a95d327f6df77d9a807453a97d95e06
iammohit-kenshin <iamnotmohit.1@gmail.com>
2026-02-15 12:05:31 +0530

    Delete cache.py

--- cache.py
+++
@@ -1,9 +0,0 @@
-# cache.py
-
-cache_db = {}
-
-def get_cached(url, quality):
-    return cache_db.get(f"{url}_{quality}")
-
-def save_cache(url, quality, file_id):
-    cache_db[f"{url}_{quality}"] = file_id
